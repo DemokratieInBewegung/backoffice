74fff7979c0da514584581054548133c9a74f078
Benjamin Kampmann <ben@gnunicorn.org>
2018-02-15 14:31:36 +0100

    first steps for our system: editable index

--- app/main/views.py
+++ app/main/views.py
@@ -6,11 +6,6 @@ from . import main
 
 @main.route('/')
 def index():
-    return render_template('main/index.html')
-
-
-@main.route('/about')
-def about():
-    editable_html_obj = EditableHTML.get_editable_html('about')
-    return render_template('main/about.html',
+    editable_html_obj = EditableHTML.get_editable_html('index')
+    return render_template('main/index.html',
                            editable_html_obj=editable_html_obj)
