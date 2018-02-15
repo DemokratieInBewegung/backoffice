2caf8c81876322b5f2976a83d1716190cd8c1122
Benjamin Kampmann <ben@gnunicorn.org>
2018-02-15 14:39:39 +0100

    add press link generator

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,9 @@ def index():
     editable_html_obj = EditableHTML.get_editable_html('index')
     return render_template('main/index.html',
                            editable_html_obj=editable_html_obj)
+
+@main.route('/stools/<tool>')
+def stools(tool):
+    return render_template('static_tools/{}.html'.format(tool))
+
+
