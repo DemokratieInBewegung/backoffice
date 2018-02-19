2a12053eaa759480448707cb0e76071d87e90cce
Benjamin Kampmann <ben@gnunicorn.org>
2018-02-19 17:19:36 +0100

    add global state

--- app/models/miscellaneous.py
+++ app/models/miscellaneous.py
@@ -14,3 +14,16 @@ class EditableHTML(db.Model):
         if editable_html_obj is None:
             editable_html_obj = EditableHTML(editor_name=editor_name, value='')
         return editable_html_obj
+
+
+class GlobalState(db.Model):
+    key = db.Column(db.String(100), primary_key=True)
+    value = db.Column(db.Text)
+
+    @staticmethod
+    def get_state(key, default=''):
+        obj = GlobalState.query.filter_by(key=key).first()
+
+        if obj is None:
+            obj = GlobalState(key=key, value=default)
+        return obj 
